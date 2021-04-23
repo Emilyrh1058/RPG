e97931f8bb42cf8838fe2e8b260752135c346f1a
Emily Salinas <emilyrh1058@gmail.com>
2021-04-22 23:06:09 -0500

    created Enemy

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -29,7 +29,6 @@ test('gets inventory from player or returns false', () => {
 
 test("gets player's health value", () => {
     const player = new Player('Dave');
-
     expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
 });
 
